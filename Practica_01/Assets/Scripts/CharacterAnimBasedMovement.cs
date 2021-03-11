74ad764b1b437d579f22a78ec116c5d3c96e86d2
moraosushi <eigna_98@hotmail.com>
2021-03-11 00:43:10 -0500

    Practica 2 V2

--- Practica_01/Assets/Scripts/CharacterAnimBasedMovement.cs
+++ Practica_01/Assets/Scripts/CharacterAnimBasedMovement.cs
@@ -11,10 +11,14 @@ public class CharacterAnimBasedMovement : MonoBehaviour
     [Range(0, 180f)]
     public float degreesToTurn = 160f;
 
+   
+    public int idleTime = 0;
+
     [Header("Animator Parameters")]
     public string motionParam = "motion";
     public string mirrorIdleParam = "mirrorIdle";
     public string turn180Param = "turn180";
+    public string idleParam = "idle_type";
 
     [Header("Animation Smoothing")]
     [Range(0, 1f)]
@@ -29,6 +33,7 @@ public class CharacterAnimBasedMovement : MonoBehaviour
     private Animator animator;
     private bool mirrorIdle;
     private bool turn180;
+   
 
     void Start()
     {
@@ -40,6 +45,28 @@ public class CharacterAnimBasedMovement : MonoBehaviour
     {
         //calculate the input magnitude
         Speed = new Vector2(hInput, vInput).normalized.sqrMagnitude;
+
+        //jump input
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetTrigger("jump");
+
+        }
+        //animaciones idle random en periodo de tiempo
+        if (!Input.anyKey)
+        {
+            idleTime = idleTime + 1;
+            print(idleTime);
+        }
+        else
+        {
+            idleTime = 0;
+        }
+        if (idleTime == Random.Range(450, 1200))
+        {
+            animator.SetFloat("idle_type", Random.Range(0, 5));
+        }
+      
 
         //dash only if character has reached maxSpeed (animator parameter value)
         if (Speed >= Speed - rotationThreshold && dash)
@@ -61,14 +88,29 @@ public class CharacterAnimBasedMovement : MonoBehaviour
 
             //rotate the character towards desired move direction based on player input and camera position
             desiredMoveDirection = forward * vInput + right * hInput;
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(desiredMoveDirection), rotationSpeed * Time.deltaTime);
 
+                               
+            if (Vector3.Angle(transform.forward, desiredMoveDirection) >= degreesToTurn)
+            {
+                turn180 = true;
+            }
+            else
+            {
+                turn180 = false;
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(desiredMoveDirection), rotationSpeed * Time.deltaTime);
+            }
+          
+            //180 turning
+            animator.SetBool(turn180Param, turn180);
+            //move character
+            animator.SetFloat(motionParam, Speed, StartAnimTime, Time.deltaTime);
         }
         else if (Speed < rotationThreshold)
         {
             animator.SetBool(mirrorIdleParam, mirrorIdle);
             animator.SetFloat(motionParam, Speed, StopAnimTime, Time.deltaTime);
         }
+   
     }
     private void OnAnimatorIK(int layerIndex)
     {
@@ -87,4 +129,5 @@ public class CharacterAnimBasedMovement : MonoBehaviour
             mirrorIdle = false;
         }
     }
+    
 }
